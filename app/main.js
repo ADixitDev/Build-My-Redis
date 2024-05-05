40142f67bbd8ddabce799ba8904045605ddef156
Aman Dixit <ad8720313@gmail.com>
2024-05-05 19:50:49 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -6,7 +6,9 @@ const net = require("net");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
-//nknn
+   connection.on("data" , (data)=>{
+    connection.write("+PONG\r\n");
+   })
 });
-//
+//port , IP Address 
  server.listen(6379, "127.0.0.1");
