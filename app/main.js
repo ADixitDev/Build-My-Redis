04543873615857a7b9ff99d09c423836e1ff9dc9
Aman Dixit <ad8720313@gmail.com>
2024-05-12 15:40:48 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -2,13 +2,14 @@ const net = require("net");
 const Parser = require("./parser");
  const Runner = require("./Runner.js");
 
-const parser = new Parser();
-const runner = new Runner();
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 //console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
+  const parser = new Parser();
+const runner = new Runner();
   // Handle connection
     connection.on("data" , (input)=>{
       const data = input.toString();
