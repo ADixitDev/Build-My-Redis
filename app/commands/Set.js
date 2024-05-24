a209068cd1eb2bc29685ab4e80574b43997667f4
Aman Dixit <ad8720313@gmail.com>
2024-05-24 17:25:16 +0530

    pass stage

--- app/commands/Set.js
+++ app/commands/Set.js
@@ -6,10 +6,30 @@ module.exports = class Set {
  execute(commands){
     if(commands.length<3) throw new Error ('Invalid commands') ; 
 
-    let store = new Store({}) ; 
-    store.data[commands[1]] = commands[2] ;
+         let [_, key, value, __ , expiry] = commands;
+ 
+         let store = new Store();
+         store.data[key] = {value}
+     
+         if(expiry) {
+           const time = new Date().getTime();
+           let expiryTime = time + parseInt(expiry);
+           
+           store.data[key].expiry = expiryTime;
+     
+           setTimeout(() => {
+             this.delete(key);
+           }, parseInt(expiry));
+         }
+    // let store = new Store({}) ; 
+    // store.data[commands[1]] = commands[2] ;
     let parser = new StringParser() ; 
     return parser.serialize("OK") ;
 
  }
+
+ delete(key) {
+        let store = new Store();
+        delete store.data[key];
+       }
 }
