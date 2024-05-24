9ccdb47b0b9c2f62113b76d0e341e556a8c3cf8a
Aman Dixit <ad8720313@gmail.com>
2024-05-24 17:10:11 +0530

    pass stage

--- app/commands/index.js
+++ app/commands/index.js
@@ -1,6 +1,8 @@
 const Echo = require('./Echo');
  const Ping = require('./PING');
  const Command = require('../commands/commads.js');
+ Const Set = require('../commands/Set.js');
+ const Get = require('../commands/Get.js');
  
  module.exports = {
    Echo, 
