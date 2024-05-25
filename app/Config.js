625c1a24787fb99812bbfee8820e21eeb448c466
Aman Dixit <ad8720313@gmail.com>
2024-05-25 17:51:06 +0530

    pass stage

--- app/Config.js
+++ app/Config.js
@@ -1,37 +1,37 @@
-module.exports= class Config{
-    static instance ;
-    constructor (config){
-        if(!Config.instance){
-            this.port = 6379 // it's default port 
-            this.replication = {
-                'role' : 'master' 
-            }
-
-            this.processConfig(config);
-            Config.instance = this ;
-            return this ;
-        }
-        return Config.instance ;
-    }
-
-
-    processConfig(args){
-     let portIdx = args.indexOf('--port');
-     if(portIdx!==-1){
-        this.port = parseInt(args[portIdx]) ; 
-     }
-
-     // Find out the replicas idx
-     let replicasIdx = args.indexOf('--replicaof');
+module.exports = class Config {
+       static instance;
+       
+       constructor(config) {
+         if(Config.instance) {
+           return Config.instance;
+         }
      
-     if(replicaIdx !== -1) {
-               this.replication = args[replicaIdx + 1];
-               this.replication = {
-                 role: 'slave',
-                 host: args[replicaIdx + 1],
-                 port: parseInt(args[replicaIdx + 2]),
-               }
-             }
-    
-    }
-}
+         // Defaults:
+         this.port = 6379
+         this.replication = {
+           'role': 'master'
+         }
+     
+         this.processConfig(config);
+         Config.instance = this;
+         return this;
+       }
+     
+       processConfig(args) {
+         // Looking for given port number
+         let portIdx = args.indexOf('--port')
+         if(portIdx !== -1) 
+           this.port = parseInt(args[portIdx + 1])
+     
+         // Looking for replica
+         let replicaIdx = args.indexOf('--replicaof')
+         if(replicaIdx !== -1) {
+           this.replication = args[replicaIdx + 1];
+           this.replication = {
+             role: 'slave',
+             host: args[replicaIdx + 1],
+             port: parseInt(args[replicaIdx + 2]),
+           }
+         }
+       }
+     }
