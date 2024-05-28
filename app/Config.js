42a30277a60b0e714d509920ac0cd03d4ff54fec
Aman Dixit <ad8720313@gmail.com>
2024-05-28 15:16:12 +0530

    pass stage

--- app/Config.js
+++ app/Config.js
@@ -12,7 +12,7 @@ module.exports = class Config {
          }
      
          // Defaults:
-         this.port = 6380 ;
+         this.port = 6379;
          this.replication = {
            'role': 'master',
            'master_replid' :randomString(40)  ,
@@ -25,20 +25,24 @@ module.exports = class Config {
        }
      
        processConfig(args) {
+        console.log('Processing config arguments:', args); 
          // Looking for given port number
          let portIdx = args.indexOf('--port')
-         if(portIdx !== -1) 
-           this.port = parseInt(args[portIdx + 1])
-     
+         if(portIdx !== -1) {
+           this.port = parseInt(args[portIdx + 1]);
+           console.log('Configured port:', this.port);  
+         }
          // Looking for replica
          let replicaIdx = args.indexOf('--replicaof')
          if(replicaIdx !== -1) {
            this.replication = args[replicaIdx + 1];
+           console.log('Configured port:', this.port);
            this.replication = {
              role: 'slave',
              host: args[replicaIdx + 1],
              port: parseInt(args[replicaIdx + 2]),
            }
+           console.log('Replication settings:', this.replication);
          }
        }
      }
