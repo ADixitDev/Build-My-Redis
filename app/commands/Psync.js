60fd468aaf713468a96d2ae02f35db5c13494968
Aman Dixit <ad8720313@gmail.com>
2024-05-28 15:39:18 +0530

    pass stage

--- app/commands/Psync.js
+++ app/commands/Psync.js
@@ -1,11 +1,14 @@
 const {StringParser}  = require ("../parser") 
+const Config = require("../Config");
 module.exports = class Psync{
     execute (commands){
         let section ;
         if(commands.length >1){
             section = commands[1] ;
             let parser = new StringParser() ; 
-           return parser.serialize("FULLRESYNC <REPL_ID> 0");
+           //return parser.serialize("FULLRESYNC <REPL_ID> 0");
+           const config = new Config();
+    return parser.serialize(`FULLRESYNC ${config.replication.master_replid} 0`);
 
         }
     }
