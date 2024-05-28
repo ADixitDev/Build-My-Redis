7a0c830205e2b49bc73f65f598a082feb0c68573
Aman Dixit <ad8720313@gmail.com>
2024-05-28 15:25:15 +0530

    pass stage

--- app/Config.js
+++ app/Config.js
@@ -36,7 +36,7 @@ module.exports = class Config {
          let replicaIdx = args.indexOf('--replicaof')
          if(replicaIdx !== -1) {
            this.replication = args[replicaIdx + 1];
-           const [host, port] = replica.host.split(' ');
+           const [host, port] = replication.split(' ');
            console.log('Configured port:', this.port);
            this.replication = {
              role: 'slave',
